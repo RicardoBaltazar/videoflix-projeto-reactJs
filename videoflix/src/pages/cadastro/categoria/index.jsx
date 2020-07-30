c912554e946a1d9ab8b48f4a024fcdbc8b7ba306
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-07-30 14:59:06 -0300

    refatorando formField / prototype / index categoria

--- videoflix/src/pages/cadastro/categoria/index.jsx
+++ videoflix/src/pages/cadastro/categoria/index.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/pageDefault/index';
-import FormField from '../../../components/FormField/index'
+import PageDefault from '../../../components/pageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button/index'
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -28,12 +29,30 @@ function CadastroCategoria() {
     );
   }
 
+  // ============
+
+  useEffect(() => {
+    if(window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias'; 
+      fetch(URL)
+       .then(async (respostaDoServer) =>{
+        if(respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategorias(resposta);
+          return; 
+        }
+        throw new Error('Não foi possível pegar os dados');
+       })
+    }    
+  }, []);
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria: {values.nome}</h1>
 
       <form onSubmit={function handleSubmit(infosDoEvento) {
           infosDoEvento.preventDefault();
+
           setCategorias([
             ...categorias,
             values
@@ -88,9 +107,9 @@ function CadastroCategoria() {
           </label>
         </div> */}
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
       
 
@@ -98,7 +117,7 @@ function CadastroCategoria() {
         {categorias.map((categoria, indice) => {
           return (
             <li key={`${categoria}${indice}`}>
-              {categoria.nome}
+              {categoria.titulo}
             </li>
           )
         })}
